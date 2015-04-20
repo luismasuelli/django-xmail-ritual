b0c93da7616f3b00b50ade88013cdbab321332ff
luismasuelli <luismasuelli@hotmail.com>
2015-04-19 21:14:00 -0500

    Version 0.0.3 con los mensajes ya compilados (no se refleja en el git)

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='django-xmail-ritual',
-    version='0.0.2',
+    version='0.0.3',
     namespace_packages=['grimoire', 'grimoire.django'],
     packages=find_packages(),
     url='https://github.com/luismasuelli/django-xmail-ritual',
